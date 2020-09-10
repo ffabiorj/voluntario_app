7213e27f50fa48940295a1df16ca74d9b347dc3f
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-10 13:43:20 -0300

    Criado as urls da api.

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from core import views as view
+
+urlpatterns = [
+    path("voluntario/", view.VoluntarioLista.as_view(), name="voluntario_lista"),
+    path(
+        "voluntario/<int:pk>/",
+        view.VoluntarioDetalhe.as_view(),
+        name="voluntario_detalhe",
+    ),
+    path("acoes/", view.AcoesLista.as_view(), name="acoes_lista"),
+    path("acoes/<int:pk>/", view.AcaoDetalhe.as_view(), name="acao_detalhe"),
+]
