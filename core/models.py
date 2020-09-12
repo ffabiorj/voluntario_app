5949637475f0e1c252c185e587c4b198486b4923
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-12 11:43:02 -0300

    modificado models.

--- core/models.py
+++ core/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Acao(models.Model):
-    nome_acao = models.CharField(max_length=100, blank=False, null=False)
-    instituicao = models.CharField(max_length=100, blank=False, null=False)
-    local = models.CharField(max_length=100, blank=False, null=False)
-    descricao = models.CharField(max_length=255, blank=False, null=False)
+    nome_acao = models.CharField(max_length=100)
+    instituicao = models.CharField(max_length=100)
+    local = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nome_acao
 
 
 class Voluntario(models.Model):
-    nome = models.CharField(max_length=50, blank=False, null=False)
-    sobrenome = models.CharField(max_length=100, blank=False, null=False)
-    bairro = models.CharField(max_length=50, blank=False, null=False)
-    cidade = models.CharField(max_length=50, blank=False, null=False)
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=100)
+    bairro = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
     acao = models.ForeignKey(Acao, related_name="voluntarios", on_delete=models.PROTECT)
 
     def __str__(self):
